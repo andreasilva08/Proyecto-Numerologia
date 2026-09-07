79c662adb35d57626de6bb61fc272d87da241918
andreasilva08 <andreasilva956379@gmail.com>
2026-09-07 11:09:15 -0500

    Update numerology.routes.js

--- routes/numerology.routes.js
+++ routes/numerology.routes.js
@@ -11,13 +11,14 @@ import {
   validarPerfilId 
 } from '../validators/numerologyProfile.validator.js';
 import { validateResult } from '../middlewares/validateResult.middleware.js';
+import { validarJWT } from '../middlewares/validarToken.js';
 
 const router = Router();
 
-router.post('/', validarCrearPerfilNumerologico, validateResult, createNumerologyProfile);
-router.get('/', getNumerologyProfiles);
-router.get('/:id', validarPerfilId, validateResult, getNumerologyProfileById);
-router.put('/:id', validarPerfilId, validarCrearPerfilNumerologico, validateResult, updateNumerologyProfile);
-router.delete('/:id', validarPerfilId, validateResult, deleteNumerologyProfile);
+router.post('/', validarJWT, validarCrearPerfilNumerologico, validateResult, createNumerologyProfile);
+router.get('/', validarJWT, getNumerologyProfiles);
+router.get('/:id', validarJWT, validarPerfilId, validateResult, getNumerologyProfileById);
+router.put('/:id', validarJWT, validarPerfilId, validarCrearPerfilNumerologico, validateResult, updateNumerologyProfile);
+router.delete('/:id', validarJWT, validarPerfilId, validateResult, deleteNumerologyProfile);
 
 export default router;
